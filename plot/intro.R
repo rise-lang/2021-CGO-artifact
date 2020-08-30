1f731e414f9be53fa6a8b71a4ebac5f59381f0b6
Thomas Koehler <basta.t.k+git@gmail.com>
2020-08-30 21:05:00 +0100

    intro plot update

--- plot/intro.R
+++ plot/intro.R
@@ -6,8 +6,9 @@ source("plot/core.R")
 
 data <- read_delim(input, " ")
 
-variants = c("halide", "lift", "reg-rot")
+variants = c("halide", "lift", "cbuf+rrot")
 data <- data %>%
+  filter(size == "1536x2560") %>%
   filter(processor == "Cortex A53") %>%
   filter(variant %in% variants) %>%
   mutate(speedup = median_ms[1]/median_ms) %>%
@@ -18,17 +19,18 @@ print(data)
 g <- ggplot(data, aes(x=variant, y=speedup, fill=generator)) +
   scale_y_continuous(name = "relative runtime\n performance") +
   geom_col(colour = "black") + # show.legend = FALSE,
-  geom_hline(yintercept = 1) +
-  geom_text(aes(label=sprintf("%0.2f", round(speedup, digits = 2))), position=position_dodge(width=0.9), vjust=-0.25) +
+  # geom_hline(yintercept = 1) +
+  geom_text(aes(label=sprintf("%0.2f", round(speedup, digits = 2))), colour="white", family="DejaVu Sans", size=4, position=position_dodge(width=0.9), vjust=1.5) +
   # facet_wrap(~processor, scales="free_y", nrow=1) +
   # coord_flip() +
   theme_bw() + theme(
     legend.title = element_blank(),
+    legend.text = element_text(size=10, family="DejaVu Sans"),
     axis.text.x = element_blank(),
     axis.text.y = element_blank(),
     axis.title.x = element_blank(),
     axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)),
-    text = element_text(size=14, family="DejaVu Sans")
+    text = element_text(size=10, family="DejaVu Sans")
   ) +
   scale_fill_manual(values = c("#882255", "#DDCC77", "#117733"))
-ggsave(output, plot = g, width = 12, height = 8, units = "cm")
+ggsave(output, plot = g, width = 8, height = 3, units = "cm")
